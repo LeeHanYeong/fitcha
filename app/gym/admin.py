eec80b5b604b78eaacc4d5024cb7940daaa41c88
LeeHanYeong <dev@lhy.kr>
2019-06-30 05:51:37 +0900

    admin update

--- app/gym/admin.py
+++ app/gym/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import (
     Gym,
@@ -20,12 +21,43 @@ class ServiceInline(admin.TabularInline):
 
 @admin.register(Gym)
 class GymAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location', 'phone_number')
+    list_display = (
+        'name', 'location', 'phone_number',
+        'images', 'facilities', 'services',
+    )
     inlines = [
         GymImageInline,
         ServiceInline,
     ]
 
+    def images(self, obj):
+        return format_html(
+            ''.join([
+                f'<img src="{image.img.url}" style="'
+                f'max-width: 200px;'
+                f'max-height: 200px;'
+                f'margin-right: 10px;'
+                f'border-radius: 5px;">'
+                for image in obj.image_set.all()
+            ]))
+    images.short_description = '이미지 목록'
+
+    def facilities(self, obj):
+        return ', '.join([
+            facility.name for facility in obj.facility_set.all()])
+
+    facilities.short_description = '시설 목록'
+
+    def services(self, obj):
+        return format_html(
+            '<br>'.join([
+                '<div>{type} ({price:,}원)</div>'.format(
+                    type=service.get_type_display(),
+                    price=service.cost,
+                ) for service in obj.service_set.all()
+            ])
+        )
+    services.short_description = '서비스 목록'
 
 @admin.register(GymImage)
 class GymImageAdmin(admin.ModelAdmin):
